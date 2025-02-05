f8fe4b14cdce2870fdbe2975e3e7ade3d57d6246
Fodi999 <fodi85999@gmail.com>
2025-02-05 12:09:01 +0100

    Описание изменений

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,17 @@ mod handlers;
 mod models;
 mod nlp;
 
-use actix_web::{App, HttpServer};
 use actix_files::Files;
+use actix_web::{App, HttpServer};
+use std::thread;
+use std::time::Duration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Запуск сервера на 0.0.0.0:8080");
-    HttpServer::new(|| {
+
+    // Запускаем сервер
+    let server = HttpServer::new(|| {
         App::new()
             // Инициализируем маршруты для WebSocket
             .configure(handlers::init_routes)
@@ -18,6 +22,19 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/", "./static").index_file("index.html"))
     })
     .bind("0.0.0.0:8080")?
-    .run()
-    .await
+    .run();
+
+    // Параллельно запускаем задачу, которая подождёт 1 секунду и откроет браузер.
+    thread::spawn(|| {
+        // Ждём немного, чтобы сервер точно запустился
+        thread::sleep(Duration::from_secs(1));
+        if webbrowser::open("http://localhost:8080").is_ok() {
+            println!("Браузер успешно открыт!");
+        } else {
+            println!("Не удалось открыть браузер.");
+        }
+    });
+
+    // Ожидаем завершения работы сервера (он будет работать до его остановки)
+    server.await
 }
